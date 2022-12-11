2c71e4dab4a0713658428557d5d55badf9c6e803
Dhananjay Mohnot <dhananjaymohnot@gmail.com>
2022-12-12 00:46:33 +0530

    Message conversation list with simple animation on click.

--- app/src/main/java/jetpack/compose/sample/SimpleMessageActivity.kt
+++ app/src/main/java/jetpack/compose/sample/SimpleMessageActivity.kt
@@ -4,14 +4,18 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -26,7 +30,8 @@ class SimpleMessageActivity : ComponentActivity() {
         setContent {
             JetpackComposesampleTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(msg = Message("Android", "Jetpack Compose"))
+                    Conversation(messages = SampleData.conversationSample)
+                    //MessageCard(msg = Message("Android", "Jetpack Compose"))
                 }
             }
         }
@@ -66,7 +71,15 @@ fun MessageCard(msg: Message) {
                 .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
@@ -74,13 +87,37 @@ fun MessageCard(msg: Message) {
             )
             Spacer(modifier = Modifier.width(8.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium, elevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     style = MaterialTheme.typography.body2,
-                    modifier = Modifier.padding(4.dp)
+                    modifier = Modifier.padding(4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1
                 )
             }
         }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        messages.map { item { MessageCard(msg = it) } }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    JetpackComposesampleTheme {
+        Conversation(messages = SampleData.conversationSample)
     }
 }
