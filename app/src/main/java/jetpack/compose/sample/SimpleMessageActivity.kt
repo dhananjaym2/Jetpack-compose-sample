6f33ad3685dddb0078e42467d740e14f4daf6041
Dhananjay Mohnot <dhananjaymohnot@gmail.com>
2022-12-11 18:32:42 +0530

    Material theming in the elements of Simple Message activity.

--- app/src/main/java/jetpack/compose/sample/SimpleMessageActivity.kt
+++ app/src/main/java/jetpack/compose/sample/SimpleMessageActivity.kt
@@ -1,11 +1,15 @@
 package jetpack.compose.sample
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,39 +18,69 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import jetpack.compose.sample.ui.theme.JetpackComposesampleTheme
 
 class SimpleMessageActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(msg = Message("Android", "Jetpack Compose"))
+            JetpackComposesampleTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(msg = Message("Android", "Jetpack Compose"))
+                }
+            }
         }
     }
 }
 
 data class Message(val author: String, val body: String)
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode",
+)
 @Composable
-fun PreviewMessageCard(s: String = "Hi") {
-    MessageCard(Message(author = "Colleague", body = "Hey look at the Jetpack Compose. It's great"))
+fun PreviewMessageCard() {
+    JetpackComposesampleTheme {
+        Surface {
+            MessageCard(
+                Message(
+                    author = "Colleague", body = "Hey look at the Jetpack Compose. It's great"
+                )
+            )
+        }
+    }
 }
 
 @Composable
 fun MessageCard(msg: Message) {
-    Row {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(R.drawable.ic_launcher_foreground),
             contentDescription = stringResource(R.string.app_name),
             modifier = Modifier
                 .size(30.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
             Spacer(modifier = Modifier.width(8.dp))
-            Text(text = msg.body)
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(4.dp)
+                )
+            }
         }
     }
 }
